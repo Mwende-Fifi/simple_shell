d313ed5414d60f5bc804a1a39da9375e86f298c7
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2023-07-21 12:39:50 +0300

    New README

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_H_
+#define _SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <strings.h>
+#include<limits.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/*Prototypes and function definations 
+go here */
+
+#endif
